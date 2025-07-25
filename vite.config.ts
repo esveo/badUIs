de9b2560e71cc94e1e538215255e097739bf10d9
Paul Dittrich <paul.dittrich@esvo.com>
2025-07-25 16:08:09 +0200

    add github pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/baduis/', // Replace 'baduis' with your repository name
 });
