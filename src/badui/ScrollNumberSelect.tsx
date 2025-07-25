8536afe59973b4c2d9a6319f3b3e7a1417b5b33c
Johann Roth <johann.roth@esveo.com>
2025-07-25 14:45:26 +0200

    adds hints to ScrollNumberSelect

--- src/badui/ScrollNumberSelect.tsx
+++ src/badui/ScrollNumberSelect.tsx
@@ -2,13 +2,24 @@ import { useState } from "react";
 
 export function ScrollNumberSelect() {
   const [value, setValue] = useState(0);
+  const [isActive, setIsActive] = useState(false);
   return (
     <div className="flex flex-row items-center">
-      <div className="mr-4">ZIP Code (scroll on yellow box to select value):</div>
-      <input type="number" value={value} readOnly className="text-right border-gray-500 border-2" />
+      <div className="mr-4">ZIP Code:</div>
+      <input
+        onClick={() => {
+          setIsActive(true);
+          window.alert("Scroll on yellow box to change value");
+        }}
+        type="text"
+        value={value.toFixed(0).padStart(5, "0")}
+        readOnly
+        className="text-right border-gray-500 border-2 pr-4"
+      />
       <div
-        className="w-[15px] h-[15px] overflow-y-scroll bg-amber-300"
+        className={`w-[15px] h-[15px] overflow-y-scroll ${isActive ? "bg-yellow-300" : ""}`}
         title="Scroll to change value"
+        onClick={() => window.alert("You need to scroll here and not click!")}
         onScroll={(e) => {
           const scrollTop = e.currentTarget.scrollTop;
           setValue(Math.round(scrollTop));
