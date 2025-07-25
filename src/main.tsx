778e8d4bd99b61778188e7ba233af0d1f2592214
Paul Dittrich <paul.dittrich@esvo.com>
2025-07-25 16:09:37 +0200

    doesnt work. revert "add github pages setup"
    This reverts commit de9b2560e71cc94e1e538215255e097739bf10d9.

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/baduis">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
